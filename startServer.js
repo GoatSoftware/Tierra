b95b9a8a7d99d9dee8e6dd4aecf67ba91bcc841b
Charly <set.charly@gmail.com>
2015-11-08 00:10:35 +0100

    [ADD] Prueba servidor node

---
+++ startServer.js
@@ -0,0 +1,66 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var server;
+
+server = http.createServer(function(req, res) {
+  // your normal server code
+  var path = url.parse(req.url).pathname;
+  switch (path) {
+    case '/':
+      fs.readFile(__dirname + 'client/index.html', function(err, data) {
+        if(err) {
+          return send404(res);
+        }
+        res.writeHead(200, {
+          'Content-Type': 'text/html'
+        });
+        res.write(data, 'utf8');
+        res.end();
+      });
+      break;
+    case '/credits.html':
+      fs.readFile(__dirname + 'client/' + path, function(err, data) {
+        if (err) {
+          return send404(res);
+        }
+        res.writeHead(200, {
+          'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'
+        });
+        res.write(data, 'utf8');
+        res.end();
+      });
+      break;
+    default:
+      send404(res);
+  }
+});
+
+var send404 = function(res) {
+  res.writeHead(404);
+  res.write('404');
+  res.end();
+};
+
+server.listen(80);
+
+// use socket.io
+var io = require('socket.io').listen(server);
+
+//turn off debug
+io.set('log level', 1);
+
+// define interactions with client
+io.sockets.on('connection', function(socket) {
+  //send data to client
+  setInterval(function() {
+    socket.emit('date', {
+      'date': new Date()
+    });
+  }, 1000);
+
+  //recieve client data
+  socket.on('client_data', function(data) {
+    process.stdout.write(data.letter);
+  });
+});
