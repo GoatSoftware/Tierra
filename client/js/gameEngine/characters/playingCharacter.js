c1e38ab56a0253a34c1d47a8d43c597c7fa632c0
Charly <set.charly@gmail.com>
2015-11-14 13:34:19 +0100

    [UPDATE] Movimiento de MainCharacter y cambio de target en camara

--- client/js/gameEngine/characters/playingCharacter.js
+++ client/js/gameEngine/characters/playingCharacter.js
@@ -6,6 +6,8 @@ TIERRA.PlayingCharacter = function() {
   //Expose methods
   self.getModel = getModel;
   self.onlineTime = onlineTime;
+  self.getPosition = getPosition;
+  self.setPosition = setPosition;
   self.move = move;
 
   //Init
@@ -22,27 +24,91 @@ TIERRA.PlayingCharacter = function() {
   //Private methods
   function constructor() {
     jumping = false;
+    speed = 1;
     position = {
       x: 0,
       y: 0,
       z: 0
     };
-    resource = TIERRA.resources.getResource('playingCharacter');
+    resource = new TIERRA.resources.Resource('playingCharacter');
   }
 
   function getModel() {
-    return resource.getModel();
+    return resource;
   }
 
   function onlineTime() {
 
   }
 
-  function move() {
+  function move(deltaTime, heading) {
+    heading = new THREE.Vector2(heading.x, heading.z);
+    var direction = TIERRA.resources.Input.getInput();
+    var deltaMove = {x: 0, y: 0, z: 0};
+    var angle = 0;
+    var code = direction.x.toString() + direction.z.toString();
+    switch (code) {
+      case "00":
+        angle = 0;
+        break;
+      case "10":
+        angle = 0;
+        break;
+      case "11":
+        angle = 45;
+        break;
+      case "01":
+        angle = 90;
+        break;
+      case "-11":
+        angle = 135;
+        break;
+      case "-10":
+        angle = 180;
+        break;
+      case "-1-1":
+        angle = 225;
+        break;
+      case "0-1":
+        angle = 270;
+        break;
+      case "1-1":
+        angle = 315;
+        break;
+    }
+    if(code != "00") {
+      movement = deltaTime * speed * 100;
+      heading.rotateAround({
+        x: 0,
+        y: 0
+      }, angle * Math.PI / 180);
+      heading.normalize();
+      deltaMove.x = heading.x;
+      deltaMove.z = heading.y;
+      deltaMove.x *= movement;
+      deltaMove.z *= movement;
+      deltaMove = {x: deltaMove.x, y: 0, z:deltaMove.z};
+      setPosition({x: position.x + deltaMove.x, y: 0, z: position.z + deltaMove.z});
+    }
+    return deltaMove;
+    // return new THREE.Vector3(x, y, z);
+  }
 
+  function getPosition() {
+    return position;
+  }
+
+  function setPosition(pos) {
+    position.x = pos.x;
+    position.y = pos.y;
+    position.z = pos.z;
+
+    resource.position.x = pos.x;
+    resource.position.y = pos.y;
+    resource.position.z = pos.z;
   }
 
   function jump() {
 
   }
-}
+};
