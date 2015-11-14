aef75371554010cb44e48b55b54ec15dfd3c73ea
Charly <set.charly@gmail.com>
2015-11-14 13:34:19 +0100

    [UPDATE] Movimiento de MainCharacter y cambio de target en camara

--- client/js/gameEngine/resources.js
+++ client/js/gameEngine/resources.js
@@ -1,12 +1,11 @@
 TIERRA.resources = {};
 
-TIERRA.resources.getResource = function(object) {
+TIERRA.resources.Resource = function(object) {
   var self = this;
   var model;
   var texture;
   var material;
   var error;
-  var reynolds;
   var receiveShadow;
   var castShadow;
   var type;
@@ -21,24 +20,24 @@ TIERRA.resources.getResource = function(object) {
 
   function init() {
     type = object;
-    switch(object) {
+    switch (object) {
       case 'playingCharacter':
         try {
-            material = new THREE.MeshLambertMaterial({
-                color: 0x1B32C0
-              });
-            radius = 5;
-            segments = 50;
-            rings = 50;
-            model = new THREE.Mesh(
-              new THREE.SphereGeometry(
-                radius,
-                segments,
-                rings),
-              material);
-            receiveShadow = true;
-            castShadow = true;
-        } catch(err) {
+          material = new THREE.MeshLambertMaterial({
+            color: 0x1B32C0
+          });
+          radius = 5;
+          segments = 50;
+          rings = 50;
+          model = new THREE.Mesh(
+            new THREE.SphereGeometry(
+              radius,
+              segments,
+              rings),
+            material);
+          receiveShadow = true;
+          castShadow = true;
+        } catch (err) {
           console.error('Error loading object ' + object + ': ' + err);
           var error = err;
         }
@@ -52,5 +51,142 @@ TIERRA.resources.getResource = function(object) {
     return model;
   }
 
-  return self;
+  return model;
 };
+
+var inputManager = function() {
+  var self = this;
+
+  self.getInput = getInput;
+
+  init();
+
+  //Private
+  function init() {
+    window.addEventListener('keyup', function(event) {
+      Key.onKeyup(event);
+    }, false);
+    window.addEventListener('keydown', function(event) {
+      Key.onKeydown(event);
+    }, false);
+    // window.addEventListener('mousedown', function(event) {
+    //   Mouse.onMouseDown(event);
+    // }, false);
+    // window.addEventListener('mouseup', function(event) {
+    //   Mouse.onMouseUp(event);
+    // }, false);
+    // window.addEventListener("mousewheel", function(event) {
+    //   Mouse.onMouseWheel(event);
+    // }, false);
+    window.addEventListener("contextmenu", function(e) {
+      e.preventDefault();
+    }, false);
+  }
+
+  function getInput() {
+    var direction = new THREE.Vector3(0, 0, 0);
+    if (Key.isDown(Key.A)) {
+      // move left
+      direction.z += -1;
+    }
+    if (Key.isDown(Key.D)) {
+      // move right
+      direction.z += 1;
+    }
+    if (Key.isDown(Key.W)) {
+      // move front
+      direction.x += 1;
+    }
+    if (Key.isDown(Key.S)) {
+      // move back
+      direction.x += -1;
+    }
+    if (Key.isDown(Key.SPACE)) {
+      direction.y = 1;
+    }
+    return direction;
+  }
+
+  var Key = {
+    _pressed: {},
+    A: 65,
+    W: 87,
+    D: 68,
+    S: 83,
+    F: 70,
+    SPACE: 32,
+
+    isDown: function(keyCode) {
+      return this._pressed[keyCode];
+    },
+
+    onKeydown: function(event) {
+      this._pressed[event.keyCode] = true;
+    },
+
+    onKeyup: function(event) {
+      delete this._pressed[event.keyCode];
+    }
+  };
+  var Mouse = {
+    _clicked: {},
+    _wheel: 0,
+    _moved: {
+      x: 0,
+      y: 0
+    },
+
+    leftClick: 1,
+    rightClick: 3,
+    middleClick: 2,
+    wheelUp: 3,
+    wheelDown: 4,
+
+    isPressed: function(button) {
+      return this._clicked[button];
+    },
+
+    scrolled: function() {
+      var wheel = this._wheel;
+      this._wheel = 0;
+      return wheel;
+    },
+
+    movement: function() {
+      var deltaMove = {
+        x: this._moved.x,
+        y: this._moved.y
+      };
+      this._moved = {
+        x: 0,
+        y: 0
+      };
+      return deltaMove;
+    },
+
+    onMouseDown: function(event) {
+      this._clicked[event.which] = true;
+      window.addEventListener('mousemove', Mouse.onMouseMove, false);
+    },
+
+    onMouseUp: function(event) {
+      delete this._clicked[event.which];
+      this._moved = {
+        x: 0,
+        y: 0
+      };
+      window.removeEventListener('mousemove', Mouse.onMouseMove);
+    },
+
+    onMouseMove: function(event) {
+      Mouse._moved.x += event.movementX;
+      Mouse._moved.y += event.movementY;
+    },
+
+    onMouseWheel: function(event) {
+      this._wheel += event.wheelDelta;
+    }
+  };
+};
+
+TIERRA.resources.Input = new inputManager();
