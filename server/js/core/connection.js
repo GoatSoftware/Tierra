bbe7202c2f4a9d1319f3b465c6c2327b60a861b2
Charly <set.charly@gmail.com>
2015-12-06 21:18:46 +0100

    [UPDATE] Connection a estado anterior

--- server/js/core/connection.js
+++ server/js/core/connection.js
@@ -1,4 +1,5 @@
 (function() {
+  //TODO Necesita rework
   var Connection = function() {
     var self = this;
     var conn = null;
@@ -7,41 +8,33 @@
 
     function setup(io, world) {
       // define interactions with client
-      io.sockets.on('connection', playerConnect);
-    }
-
-    function playerConnect(socket) {
-      //Actualizamos la lista de jugadores
-      //Preparamos la lista de los jugadores para enviarla
-      //Enviamos a todos los que esten alrededor un paquete para que vean a esta nueva conexión
-      var players = world.addPlayer(socket);
-      if (CONFIG.DEBUG) {
+      io.sockets.on('connection', function(socket) {
+        //Actualizamos la lista de jugadores
+        //Preparamos la lista de los jugadores para enviarla
+        //Enviamos a todos los que esten alrededor un paquete para que vean a esta nueva conexión
+        var players = world.addPlayer(socket);
         console.log("Player connected");
-      }
 
-      //send data to client
-      socket.emit('welcome_message', {
-        'players': players,
-        'texto': "Bienvenido a Brown (Emmet (Dr (AKA Doc)))"
+        //send data to client
+        socket.emit('welcome_message', {
+          'texto': "Bienvenido a Brown (Emmet (Dr (AKA Doc)))",
+          'players': players
+        });
+
+        //recieve client data
+        socket.on('type', function(data) {
+          console.log(data);
+          // process.stdout.write(data.letter);
+        });
+
+        socket.on('disconnect', function() {
+          console.log('Player disconnected');
+
+          world.removePlayer(socket);
+          // var i = allClients.indexOf(socket);
+          // allClients.splice(i, 1);
+        });
       });
-
-      //recieve client data
-      socket.on('type', handlePacket);
-
-      socket.on('disconnect', playerDisconnect);
-    }
-
-    function handlePacket(data) {
-      console.log(data);
-      // process.stdout.write(data.letter);
-    }
-
-    function playerDisconnect(socket) {
-      console.log('Player disconnected');
-
-      world.removePlayer(socket);
-      // var i = allClients.indexOf(socket);
-      // allClients.splice(i, 1);
     }
   };
 
