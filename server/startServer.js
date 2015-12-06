cee0d47e6c7ad51eb7a3a486ee0b01d84f1f34ac
Charly <set.charly@gmail.com>
2015-12-06 13:15:02 +0100

    [UPDATE] Cambio en require de ficheros

--- server/startServer.js
+++ server/startServer.js
@@ -2,7 +2,7 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var server;
-var tierra = require('./js/Tierra.js');
+var tierra = require('./js/tierra');
 var aux = "";
 
 server = http.createServer(function(req, res) {});
